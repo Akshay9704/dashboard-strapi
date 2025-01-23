6195c87c234129569ae00b9afa2d8ad8cace5cb6
Akshay Bhasin <107040611+Akshay9704@users.noreply.github.com>
2025-01-23 18:07:29 +0530

    Initialize Data

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -3,12 +3,11 @@ import type { Schema, Struct } from '@strapi/strapi';
 export interface SharedMedia extends Struct.ComponentSchema {
   collectionName: 'components_shared_media';
   info: {
+    description: '';
     displayName: 'Media';
     icon: 'file-video';
   };
-  attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
-  };
+  attributes: {};
 }
 
 export interface SharedQuote extends Struct.ComponentSchema {
